7ed32bcd8c750317ca1e6f130658a6472e366675
amichaikafka <74260591+amichaikafka@users.noreply.github.com>
2020-12-06 15:50:50 +0200

    Update myBank.h

--- myBank.h
+++ myBank.h
@@ -1,10 +1,10 @@
 #ifndef _MYBANK_
 #define _MYBANK_
 
-        //a constant valio for exponent
-    #define arrow 50           // array size for the account numbers
-    #define arrcol 2          // 2 colmmes for status of bank account and its funds
-    extern  double bank[arrow][arrcol];
+        //a constants value for the array size 
+    #define arrow 50           // 50 lines for the account numbers
+    #define arrcol 2          // 2 columns for the information about every bank account 
+    extern  double bank[arrow][arrcol]; 
     //the fanctoins that the libary myBank contain
     void O();
     void B();
